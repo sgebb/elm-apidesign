854e839db95d6093771219d6a48af00802aa9fac
Snorre <snorre.stenerud@gmail.com>
2020-04-13 20:19:16 +0200

    dragndrop med litt sære sideeffekter

--- src/Main.elm
+++ src/Main.elm
@@ -32,7 +32,8 @@ init : () -> ( Model, Cmd Msg )
 init _ =
     ( {
         ressurser = [testResource], 
-        input = ""
+        input = "",
+        movingResource = Maybe.Nothing
     }
     , Cmd.none
     )
@@ -40,7 +41,8 @@ init _ =
 
 type alias Model =
     { ressurser : List Resource,
-      input : String
+      input : String,
+      movingResource : Maybe Resource
     }
 
 type alias Resource = 
@@ -63,19 +65,17 @@ emptySubResources =
 
 
 type Msg
-    = NoOp
-    | Message
-    | UpdateInput String
+    = UpdateInput String
     | KeyDown Int
+    | Move Resource
+    | DropOn Resource
+    | CancelMove
+    | DragOver
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
-        Message ->
-            ( model, Cmd.none )
         UpdateInput word -> 
             ( {model | input = word}, Cmd.none)
         KeyDown key ->
@@ -85,10 +85,49 @@ update msg model =
                     input = "" }, Cmd.none)
             else
                 (model, Cmd.none)
+        Move resource -> 
+            ( {model | movingResource = Just resource}, Cmd.none)
+        CancelMove ->
+            ({model | movingResource = Nothing}, Cmd.none)
+        DropOn destination ->
+            ({model | ressurser = moveResource model.ressurser model.movingResource destination, movingResource = Nothing} , Cmd.none)
+        DragOver ->
+            (model, Cmd.none)
 
+moveResource : List Resource -> Maybe Resource -> Resource -> List Resource
+moveResource alleRessurser ressurs destination =
+        case ressurs of 
+            Just res ->
+                let
+                    newRessurser = List.filter (areNotTheSame res) alleRessurser
+                    withoutDest = List.filter (areNotTheSame destination) newRessurser
+                    destinationAfterMoved = 
+                        case destination.subressurser of
+                            SubResources sub ->
+                                {destination | subressurser = SubResources (res :: sub)}
+                in
+                    destinationAfterMoved :: withoutDest
+            _ ->
+                alleRessurser
+            
+
+areTheSame : Resource -> Resource -> Bool
+areTheSame first other =     
+        first.navn == other.navn &&  equalAmountSubResources first.subressurser other.subressurser
+
+areNotTheSame : Resource -> Resource -> Bool
+areNotTheSame first other = 
+    not (areTheSame first other)
+
+equalAmountSubResources : SubResources -> SubResources -> Bool
+equalAmountSubResources first other =
+    case first of
+        SubResources fi->
+            case other of 
+                SubResources ot -> 
+                    List.length fi == List.length ot
 
 -- Subscriptions
-
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -98,6 +137,26 @@ onKeyDown : (Int -> msg) -> Attribute msg
 onKeyDown tagger =
   on "keydown" (Json.map tagger keyCode)
 
+onDragStart : msg -> Attribute msg
+onDragStart message =
+    on "dragstart" (Json.succeed message)
+
+onDragEnd : msg -> Attribute msg
+onDragEnd message =
+    on "dragend" (Json.succeed message)
+
+
+onDragOver : msg -> Attribute msg
+onDragOver msg =
+    Html.Events.preventDefaultOn "dragover"
+        <| Json.succeed (msg, True)
+
+    
+onDrop : msg -> Attribute msg
+onDrop msg =
+    Html.Events.preventDefaultOn "drop"
+        <| Json.succeed (msg, True)
+
 -- View
 
 
@@ -106,8 +165,8 @@ view model =
     div []
         [
             viewResources model.ressurser,
-            input [onKeyDown KeyDown, placeholder "A word", value model.input, onInput UpdateInput][],
-            h1 [][text model.input]
+            h1 [][text "Start by writing some words.."],
+            input [onKeyDown KeyDown, placeholder "A word", value model.input, onInput UpdateInput][]
         ]
 
 viewResources : List Resource -> Html Msg
@@ -123,14 +182,20 @@ viewSubressurser subresources =
         SubResources resources ->
             viewResources resources
 
-            
-    
-
 viewResource : Resource -> Html Msg
-viewResource resource = 
-    li []
-    [
-        text resource.navn,
-        text ":",
-        viewSubressurser resource.subressurser
+viewResource resource =
+    div[
+        attribute "draggable" "true",
+        attribute "ondragover" "return false",
+        onDragEnd CancelMove,
+        onDragOver DragOver,
+        onDragStart <| Move resource,
+        onDrop <| DropOn resource]
+        [
+            li []
+        [
+            text resource.navn,
+            text ":",
+            viewSubressurser resource.subressurser
+        ]
     ]
